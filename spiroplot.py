16eef356be02890b72673bca783e4e056ac9c349
BenGauntRFI <ben.gaunt@rfi.ac.uk>
2021-11-26 12:01:22 +0000

    Created functions to plot a spirograph for different input parameters, and some code to plot the result with pyplot.

--- spiroplot.py
+++ spiroplot.py
@@ -2,3 +2,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, RangeSlider
+
+
+def phasor(theta, r):
+    """
+    Caluclates the complex exponential for given r and theta.
+    :param theta: phase angle, can be numpy array
+    :param r: magnitude, should be a float
+    :return: numpy array of complex values with magnitude r and phase angles given by thetas
+    """
+    return r * np.exp(1j * theta)
+
+def spiro_sum(theta, theta_ratios, r_vals):
+    """
+    Adds together a series of phasors to create a complex pattern.
+    :param theta: numpy array of phase angles to map over
+    :param theta_ratios: numpy array with ratios for each phasor determining how fast it rotates in relation
+    to the global theta value. 1.0 = same speed, 0.5 = half as fast, 2.0 = twice as fast
+    :param r_vals: numpy array with a radius for each phasor
+    :return: numpy array of complex values making up the spirogram.  Same size as theta.
+    """
+
+    spirograph = np.zeros(theta.shape)
+    for i in range(len(theta_ratios)):
+        phasor_vals = phasor(theta * theta_ratios[i], r_vals[i])
+        spirograph = spirograph + phasor_vals
+
+    return spirograph
+
+
+
+
+if __name__ == '__main__':
+    N = 10000 #number of points for full theta array
+    NUM_LOOPS = 25 #how many times to loop round
+
+    theta_arr = np.linspace(0, NUM_LOOPS * 2 * np.pi, N)
+
+    theta_ratios_arr = np.array([1.0, 1.3 * np.pi])
+    r_vals_arr = np.array([1.0, 0.7])
+
+    complex_spirograph = spiro_sum(theta_arr, theta_ratios_arr, r_vals_arr)
+
+    fig, ax = plt.subplots()
+    line, = ax.plot(np.real(complex_spirograph), np.imag(complex_spirograph), lw=2)
+    ax.set_xlabel('Real component')
+    ax.set_ylabel('Imaginary component')
+
+    plt.show()
+
